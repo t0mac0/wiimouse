a282f1b0b39eb06f62bc53fb817a39c7e75b6d2f
driedler <driedler@edfe3db1-16f4-af7d-7469-0a75d665ad3f>
2011-02-05 18:07:27 +0000

    Both HID mouse and Virtual com port enumerate. Rx/Tx ep addresses need to be adjusted.

---
+++ dan/hid_mouse_virtual_com/src/main.c
@@ -0,0 +1,102 @@
+
+#include "stm32f10x_lib.h"
+#include "core_cm3.h"
+#include "bits.h"
+#include "printf.h"
+#include "drv_nunchuck.h"
+#include "delay.h"
+#include "hw_config.h"
+#include "USB_Init.h"
+#include "usb_hid_mouse.h"
+#include "usb_sil.h"
+#include "usb_regs.h"
+#include "usart.h"
+#include "hw_config.h"
+#include "usb_conf.h"
+#include "usb_desc.h"
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+
+/*************************************************************************
+ * Function Name: main
+ * Parameters: none
+ * Return: Int32U
+ *
+ * Description: The main subroutine
+ *
+ *************************************************************************/
+int main(void)
+{
+	HID_MOUSE_REPORT report;
+	uint8 buffer[64];
+	uint32 size;
+
+	Set_System();
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE);
+
+	InitUSART2();
+
+	printf("DEVICE_CONFIGURATION_DESCRIPTOR_SIZE: %d\n", DEVICE_CONFIGURATION_DESCRIPTOR_SIZE);
+	printf("USB_DEVICE_DESCRIPTOR_SIZE: %d\n", USB_DEVICE_DESCRIPTOR_SIZE);
+
+
+	USB_Interrupts_Config();
+
+	Set_USBClock();
+
+
+	/* Setup SysTick Timer for 1 msec interrupts  */
+	if (SysTick_Config(SystemCoreClock / 1000))
+	{
+		/* Capture error */
+		while (1);
+	}
+
+	USB_Init();
+
+	nunchuck_init();
+
+
+	while(1)
+	{
+		if( nunchuck_read())
+		{
+			nunchuck_get_joystick(&report.X, &report.Y);
+			report.Buttons.Left = nunchuck_z_pressed();
+			report.Buttons.Right = nunchuck_c_pressed();
+
+			report.X -= 129;
+			report.Y = 130 - report.Y;
+
+			size = 0;
+			snprintf(buffer, &size,
+					"X: %d, Y: %d, Z: %d, C: %d\n",report.X, report.Y, report.Buttons.Left, report.Buttons.Right);
+
+			//USB_SIL_Write(EP1_IN, buffer, size);
+			//SetEPTxValid(ENDP1);
+
+			USB_SIL_Write(EP4_IN, &report, HID_MOUSE_REPORT_SIZE);
+			SetEPTxValid(ENDP4);
+			  /* Copy mouse position info in ENDP1 Tx Packet Memory Area*/
+			  //USB_SIL_Write(EP1_IN, (uint8*)&report, 3);
+			  //SetEPTxValid(ENDP1);
+		}
+
+	}
+}
+
+
+void assert_failed(uint8* file, uint32 line)
+{
+	printf("Assert failed: file: %s, line: %d\n", file, line);
+	while(1);
+}
+
+
+
+
+
